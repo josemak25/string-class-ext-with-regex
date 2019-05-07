a9862f93b2736a39b42702e8f8580df4dd1257ec
josemak25 <amakirij@gmail.com>
2019-05-08 00:07:57 +0100

    add test for toUpper and implemented toUpper method on string prototype

---
+++ test/resources/js/toUpper.test.js
@@ -0,0 +1,8 @@
+var toUpper = require('../../../resources/js/toUpper');
+
+describe('TEST A STRING  WITH AN LOWER CASE CHARACTER AND RETURN STRING WITH ALL CHARACTERS IN UPPERCASE', function() {
+  it('return a string of lower to uppercase', function() {
+    var result = 'ProgrAmminG'.toUpper();
+    expect(result).toBe('PROGRAMMING');
+  });
+});
