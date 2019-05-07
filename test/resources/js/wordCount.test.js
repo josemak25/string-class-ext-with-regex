82b0964462ba21fdf3707d35b0baeb47fdb0ed85
josemak25 <amakirij@gmail.com>
2019-05-07 20:39:03 +0100

    fix typo error on word count method test

--- test/resources/js/wordCount.test.js
+++ test/resources/js/wordCount.test.js
@@ -1,7 +1,7 @@
 var wordCount = require('../../../resources/js/wordCount');
 
-describe('TEST A STRING AN RETURN ALL WORDS IN THE STRING', function() {
-  it('return an array of words in string when function is called on a string', function() {
+describe('TEST A STRING AN RETURN THE NUMBER OF WORDS IN THE STRING', function() {
+  it('return a number of the words in a string when function is called on a string', function() {
     var result = 'Programmings courses always starts with hello world example'.wordCount();
     expect(result).toBe(8);
   });
